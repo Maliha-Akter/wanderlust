616c57ea57a43da23b03b217ae0d322c2c89f1ba
Maliha-Akter <maliha.miti.0276@gmail.com>
2026-06-16 11:04:05 +0600

    first commit

---
+++ src/app/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import Link from 'next/link';
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <nav>
+            <ul>
+                <li><Link href={"/"}>Home</Link></li>
+                <li><Link href={"/destinations"}>Destinations</Link></li>
+                <li><Link href={"/my-bookings"}>My Bookings</Link></li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Navbar;
